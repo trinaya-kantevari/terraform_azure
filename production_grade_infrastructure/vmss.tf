59b08f9ff3b7e4ef9b569c647afb740a49862b66
Trinaya Kantevari <108997403+trinaya-kantevari@users.noreply.github.com>
2025-12-05 03:36:43 +0530

    modified the path_to_key.pub_file part

--- production_grade_infrastructure/vmss.tf
+++ production_grade_infrastructure/vmss.tf
@@ -16,7 +16,7 @@ resource "azurerm_orchestrated_virtual_machine_scale_set" "vmss" {
       admin_username                  = "azureuser"
       admin_ssh_key {
         username   = "azureuser"
-        public_key = file("<path to key.pub file>")
+        public_key = file("<path_to_key.pub_file>")
       }
     }
   }
