645d98cfe226eb779c7c7b5c69b1782c10d23575
Trinaya Kantevari <trinayakantevari@gmail.com>
2026-01-08 16:03:24 +0530

    removed hard coded sub id and tenant id

--- aks_cluster/provider.tf
+++ aks_cluster/provider.tf
@@ -15,10 +15,10 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   resource_provider_registrations = "none"
-  subscription_id = "ba942dc7-1b04-43b7-a56d-c2578e756591"
+  subscription_id = ""
   features {}
 }
 
 provider "azuread" {
-  tenant_id = "f2a009da-b491-4dbb-94e8-5809162549cd"
+  tenant_id = ""
 }
